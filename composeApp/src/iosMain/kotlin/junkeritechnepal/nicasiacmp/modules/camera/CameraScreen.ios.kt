3411994058be5ae2782845b3725bfa1f65e88eb9
swornim.shah <swornim.shah@f1soft.com>
2025-11-21 16:02:41 +0545

    navigation3 wip setup stable library

--- composeApp/src/iosMain/kotlin/junkeritechnepal/nicasiacmp/modules/camera/CameraScreen.ios.kt
+++ composeApp/src/iosMain/kotlin/junkeritechnepal/nicasiacmp/modules/camera/CameraScreen.ios.kt
@@ -1,43 +1,10 @@
 package junkeritechnepal.nicasiacmp.modules.camera
 
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.interop.UIKitView
 import androidx.compose.ui.viewinterop.UIKitViewController
 import junkeritechnepal.nicasiacmp.LocalNativeViewProvider
-import kotlinx.cinterop.CValue
-import kotlinx.cinterop.ExperimentalForeignApi
-import kotlinx.cinterop.convert
-import platform.AVFoundation.AVAuthorizationStatusAuthorized
-import platform.AVFoundation.AVAuthorizationStatusNotDetermined
-import platform.AVFoundation.AVCaptureDevice
-import platform.AVFoundation.AVCaptureDeviceInput
-import platform.AVFoundation.AVCaptureDevicePositionBack
-import platform.AVFoundation.AVCaptureSession
-import platform.AVFoundation.AVCaptureSessionPresetPhoto
-import platform.AVFoundation.AVCaptureVideoPreviewLayer
-import platform.AVFoundation.AVLayerVideoGravityResizeAspectFill
-import platform.AVFoundation.AVMediaTypeVideo
-import platform.AVFoundation.authorizationStatusForMediaType
-import platform.AVFoundation.position
-import platform.AVFoundation.requestAccessForMediaType
-import platform.CoreGraphics.CGRect
-import platform.QuartzCore.CATransaction
-import platform.QuartzCore.kCATransactionDisableActions
-import platform.UIKit.UIView
-import platform.UIKit.UIViewContentMode
-import platform.darwin.dispatch_async
-import platform.darwin.dispatch_get_global_queue
-import platform.darwin.dispatch_get_main_queue
-import platform.posix.QOS_CLASS_USER_INITIATED
 
 @Composable
 actual fun CameraScreen() {
